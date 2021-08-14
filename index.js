7da41984653947fee29a9a8c567dabfb870a9957
Damian Sire <damiansire@hotmail.com>
2021-08-14 16:56:23 -0300

    I use code best practices :)

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var https = require('https');
+const https = require('https');
 const fs = require("fs");
+const path = require('path');
 
-let GlobalConfig = JSON.parse(fs.readFileSync("./config.json"));
-let filesLinksObj = JSON.parse(fs.readFileSync("./filesToDownload.json"));
+let globalConfig = JSON.parse(fs.readFileSync("config.json"));
+let filesLinksObj = JSON.parse(fs.readFileSync("filesToDownload.json"));
 
 function downloadFile(urlToDownload, fileName) {
-    let basePath = `${GlobalConfig.relativePathToDownload}/${getCurrentDateTime()}`;
-    createRelativeDirectoryRecursive(basePath);
-    var downloadPath = `${basePath}/ ${fileName}`;
-    var file = fs.createWriteStream(downloadPath);
+    let basePath = path.join(globalConfig.relativePathToDownload, getCurrentDateTime());
+    if (!fs.existsSync(basePath)) {
+        fs.mkdirSync(basePath, { recursive: true });
+    }
+    let downloadPath = path.join(basePath, fileName);
+    let file = fs.createWriteStream(downloadPath);
 
     https.get(urlToDownload, function (response) {
         response.pipe(file);
@@ -17,16 +20,9 @@ function downloadFile(urlToDownload, fileName) {
             console.log("DONE LOADING");
         })
     }).on("error", function (err) {
-
         console.log("ERROR " + err.message);
         fs.unlink(downloadPath);
     });
-}
-
-function directoryDontExistSoCreate(dir) {
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
 }
 
 function getCurrentDateTime() {
@@ -34,17 +30,7 @@ function getCurrentDateTime() {
     return `Day-${f.getDate()}-Month-${(f.getMonth() + 1)}-year-${f.getFullYear()}`
 }
 
-function createRelativeDirectoryRecursive(path) {
-    let pathArr = path.split("/") //[".","pepe","algo"]
-    let initialDir = "."
-    for (let index = 1; index < pathArr.length; index++) {
-        initialDir += `/${pathArr[index]}`;
-        directoryDontExistSoCreate(initialDir);
-    }
-}
-
 filesLinksObj.forEach(fileObj => {
     downloadFile(fileObj.fileUrl, fileObj.fileName);
 });
-
 
